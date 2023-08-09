42d8e143ab22b2a1315e2ae850aabf6e00bc5685
Jobayer <jobayerhossain977@gmail.com>
2023-08-09 10:05:50 +0600

    finished

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,1 @@
-<script>
-	import { i, language, switchLanguage } from '@inlang/sdk-js';
-</script>
-
-{i('welcome')}
-<br />
-{#if language === 'en'}
-	<button on:click={() => switchLanguage('de')}> Switch to German </button>
-{/if}
-{#if language === 'de'}
-	<button on:click={() => switchLanguage('en')}> Switch to English </button>
-{/if}
-
 <slot />
